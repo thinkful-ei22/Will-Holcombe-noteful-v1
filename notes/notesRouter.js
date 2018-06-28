bdcda4dd18a0ca978989f7449521a95d61471ecd
William Holcombe <william.holcombe@uconn.edu>
2018-06-28 07:34:56 -0400

    added promises to all notes.methods

--- notes/notesRouter.js
+++ notes/notesRouter.js
@@ -17,78 +17,86 @@ const notes = simDB.initialize(data); // <<== and this
 
 
 
-  router.put('/notes/:id', (req, res, next) => {
-    const id = req.params.id;
+router.put('/notes/:id', (req, res, next) => {
+  const id = req.params.id;
   
-    /***** Never trust users - validate input *****/
+  /***** Never trust users - validate input *****/
     
-    const updateObj = {};
-    const updateFields = ['title', 'content'];
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
   
-    updateFields.forEach(field => {
-      if (field in req.body) {
-        updateObj[field] = req.body[field];
-      }
-    });
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
   
-    notes.update(id, updateObj, (err, item) => {
-      if (err) {
-        return next(err);
-      }
-      if (item) {
+  notes.update(id, updateObj)
+    .then(item => {
+      if(item){
         res.json(item);
-      } else {
+      }
+      else{
         next();
       }
+    })
+      .catch(err =>{
+      next(err)
+      
     });
   });
+
+
+
 
 router.delete('/notes/:id', (req, res, next) => {
   const id = req.params.id; //same as const {id} = req.params
 
-      notes.delete( id, err => {
-        if (err) {
-         return next(err);
-           }
-        
-        else {
-         res.sendStatus(204);
-  }
-
+  notes.delete(id)
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
-});
-
-
-
-  router.post('/notes', (req, res, next) => {
     
-    const { title, content } = req.body;
+
+
+
+router.post('/notes', (req, res, next) => {
+    
+  const { title, content } = req.body;
   
-    const newItem = { title, content };
-    //console.log('Hello', newItem);
-    /***** Never trust users - validate input *****/
-    if (!newItem.title) {
-      const err = new Error('Missing `title` in request body');
-      err.status = 400;
-      return next(err);
-    }
+  const newItem = { title, content };
+  //console.log('Hello', newItem);
+  /***** Never trust users - validate input *****/
+  if (!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
   
-    notes.create(newItem, (err, item) => {
-      if (err) {
-        return next(err);
-      }
+  notes.create(newItem)
+    .then(item => {
       if (item) {
         res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
-      } else {
+      }
+      else{
         next();
       }
+    })
+    .catch(err => {
+      next(err);
     });
-  });
+});
+
+   
 
 
   
-  /*app.use(function (err, req, res, next) {
+/*app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.json({
       message: err.message,
@@ -98,34 +106,39 @@ router.delete('/notes/:id', (req, res, next) => {
   
 
   */
-  router.get('/notes', (req, res, next) => {
-    const { searchTerm } = req.query;
-  
-    notes.filter(searchTerm, (err, list) => {
-      if (err) {
-        return next(err); // goes to error handler
-      }
-      res.json(list); // responds with filtered array
+router.get('/notes', (req, res, next) => {
+  const { searchTerm } = req.query;
+  notes.filter(searchTerm)
+    .then(list => {
+      res.json(list);
+    })
+    .catch(err => {
+      next(err);
     });
-  });
+});
+
+
+
+
+ 
     
   
-  router.get('/notes/:id', (req, res) => {
+router.get('/notes/:id', (req, res) => {
   const id = req.params.id;
-  notes.find(id, (err, item) =>{
-    if(err) {
-      console.error(err);
-    }
-    if(item) {
-      res.json(item);
-    }
-    else{
-      console.log('not found');
-    }
-  });
+  notes.find(id)
+    .then(item => {
+      if (item) {
+        res.json(item);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
   
-  });
-  
+
 module.exports = router;
  
  
