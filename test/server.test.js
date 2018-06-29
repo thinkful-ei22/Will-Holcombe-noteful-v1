765a29e85aa4031a70b861661399e454897d88d7
William Holcombe <william.holcombe@uconn.edu>
2018-06-28 20:47:13 -0400

    tested get request for all notes

--- test/server.test.js
+++ test/server.test.js
@@ -7,41 +7,26 @@ const chaiHttp = require('chai-http');
 const expect = chai.expect;
 
 chai.use(chaiHttp);
+describe('notes', function() {
 
-describe('Reality check', function () {
-
-  it('true should be true', function () {
-    expect(true).to.be.true;
-  });
-
-  it('2 + 2 should equal 4', function () {
-    expect(2 + 2).to.equal(4);
-  });
-
-});
-
-describe('Express static', function () {
-
-  it('GET request "/" should return the index page', function () {
+  // `chai.request.get` is an asynchronous operation. When
+  // using Mocha with async operations, we need to either
+  // return an ES6 promise or else pass a `done` callback to the
+  // test that we call at the end. We prefer the first approach, so
+  // we just return the chained `chai.request.get` object.
+  it('should list notes on GET', function() {
     return chai.request(app)
-      .get('/')
-      .then(function (res) {
-        expect(res).to.exist;
+      .get('/api/notes')
+      .then(function(res) {
         expect(res).to.have.status(200);
-        expect(res).to.be.html;
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.above(0);
+        res.body.forEach(function(item) {
+          expect(item).to.be.a('object');
+          expect(item).to.have.all.keys(
+            'content', 'id', 'title');
+        });
       });
   });
-  
-});
-  
-describe('404 handler', function () {
-  
-  it('should respond with 404 when given a bad path', function () {
-    return chai.request(app)
-      .get('/DOES/NOT/EXIST')
-      .then(res => {
-        expect(res).to.have.status(404);
-      });
-  });
-  
 });
